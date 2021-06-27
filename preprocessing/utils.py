514b080f6b0d7454398df559c6f3e29e3955fc1b
Max Malynowsky <github@maxmalynowsky.com>
2021-06-28 01:53:06 +0200

    remove extra module

--- preprocessing/utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-DATABASE = 'adm0_template'
-LAND_URL = 'https://osmdata.openstreetmap.de/download/land-polygons-complete-4326.zip'
-ADM0_URL = 'https://data.fieldmaps.io/adm0-template'
-
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
