e16f122a26d21890586feef5b9b0c9d086b0e4ab
Max Malynowsky <github@maxmalynowsky.com>
2022-01-09 10:18:17 -0500

    reshape meta outputs

--- processing/meta.py
+++ processing/meta.py
@@ -1,7 +1,8 @@
 import json
 import pandas as pd
 from pathlib import Path
-from .utils import DATA_URL, logging, world_views, geoms, get_land_date
+from .utils import (DATA_URL, logging, prefixes,
+                    world_views, geoms, get_land_date)
 
 logger = logging.getLogger(__name__)
 cwd = Path(__file__).parent
@@ -11,33 +12,32 @@ outputs = cwd / '../outputs'
 def main():
     outputs.mkdir(exist_ok=True, parents=True)
     data = []
-    for wld in world_views:
-        for geom in geoms:
-            row = {
-                'id': f'{wld}_{geom}',
-                'wld': wld,
-                'geom': geom,
-                'date': get_land_date(),
-                'url_gpkg': f'{DATA_URL}/adm0/{wld}/adm0_{geom}.gpkg.zip',
-                'url_shp': f'{DATA_URL}/adm0/{wld}/adm0_{geom}.shp.zip',
-                'url_xlsx': f'{DATA_URL}/adm0/{wld}/adm0_{geom}.xlsx',
-                'simplified_gpkg': f'{DATA_URL}/adm0/{wld}/simplified_adm0_{geom}.gpkg.zip',
-                'simplified_shp': f'{DATA_URL}/adm0/{wld}/simplified_adm0_{geom}.shp.zip',
-                'simplified_xlsx': f'{DATA_URL}/adm0/{wld}/simplified_adm0_{geom}.xlsx',
-            }
-            data.append(row)
-    data.append({
-        'id': f'land_polygons',
-        'wld': 'land',
-        'geom': 'polygons',
-        'date': get_land_date(),
-        'url_gpkg': f'{DATA_URL}/adm0/land/land_polygons.gpkg.zip',
-        'url_shp': f'{DATA_URL}/adm0/land/land_polygons.shp.zip',
-        'url_xlsx': None,
-        'simplified_gpkg': f'{DATA_URL}/adm0/land/simplified_land_polygons.gpkg.zip',
-        'simplified_shp': f'{DATA_URL}/adm0/land/simplified_land_polygons.shp.zip',
-        'simplified_xlsx': None,
-    })
+    for prefix in prefixes:
+        for wld in world_views:
+            for geom in geoms:
+                row = {
+                    'id': f'{prefix}{wld}_adm0_{geom}',
+                    'grp': 'original' if prefix == '' else 'simplified',
+                    'wld': wld,
+                    'adm': 0,
+                    'geom': geom,
+                    'date': get_land_date(),
+                    'url_gpkg': f'{DATA_URL}/adm0/{wld}/{prefix}adm0_{geom}.gpkg.zip',
+                    'url_shp': f'{DATA_URL}/adm0/{wld}/{prefix}adm0_{geom}.shp.zip',
+                    'url_xlsx': f'{DATA_URL}/adm0/{wld}/{prefix}adm0_{geom}.xlsx',
+                }
+                data.append(row)
+        data.append({
+            'id': f'{prefix}land_polygons',
+            'grp': 'original' if prefix == '' else 'simplified',
+            'wld': 'land',
+            'adm': 0,
+            'geom': 'polygons',
+            'date': get_land_date(),
+            'url_gpkg': f'{DATA_URL}/adm0/land/{prefix}land_polygons.gpkg.zip',
+            'url_shp': f'{DATA_URL}/adm0/land/{prefix}land_polygons.shp.zip',
+            'url_xlsx': None,
+        })
     with open((outputs / f'adm0.json'), 'w') as f:
         json.dump(data, f, separators=(',', ':'))
     df = pd.DataFrame(data)
