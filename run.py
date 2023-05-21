797160f0255db81c7f752619f736a0e77191300a
Max Malynowsky <github@maxmalynowsky.com>
2023-05-21 19:03:08 +0200

    update UN M49 names

--- run.py
+++ run.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     for layer in layers:
         for ext in exts:
             (land / f"{layer}.{ext}").unlink(missing_ok=True)
-    subprocess.run(["python", "-m", "processing"])
+    subprocess.run(["python", "-m", "app"])
     subprocess.run(["python", "sync.py"])
